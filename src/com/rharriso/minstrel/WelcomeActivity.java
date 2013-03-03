235af66148d247e90cfd336382178f64deba91f9
Ross Harrison <rtharrison86@gmail.com>
2013-03-02 21:34:21 -0600

    Adding booksmakrs to a box

--- src/com/rharriso/minstrel/WelcomeActivity.java
+++ src/com/rharriso/minstrel/WelcomeActivity.java
@@ -1,26 +1,41 @@
 package com.rharriso.minstrel;
 
+import java.io.IOException;
+
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class WelcomeActivity extends Activity {
+public class WelcomeActivity extends Activity implements OnClickListener {
 
+	MediaPlayer mPlayer = new MediaPlayer();
+	Button mAddBookmarkBtn = null;
+	TextView mBookMarkList = null;
+	String mCurrTrackTitle = null;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        MediaPlayer player = new MediaPlayer();
+        super.onCreate(savedInstanceState);        
+        setContentView(R.layout.welcome);
         
         ContentResolver contentResolver = getContentResolver();
         Cursor cursor = contentResolver.query(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, 
         										null, null, null, null);
+        
+        mBookMarkList   = (TextView)findViewById(R.id.bookmark_list);
+        mAddBookmarkBtn = (Button)findViewById(R.id.add_bookmark_btn);
+        mAddBookmarkBtn.setOnClickListener(this);
         
         //fuck null shit
         Log.d("FUCK", "THAT SHIT");
@@ -29,23 +44,27 @@ public class WelcomeActivity extends Activity {
         }else{
         	int titleCol = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media.TITLE);
         	int idCol = cursor.getColumnIndex(android.provider.MediaStore.Audio.Media._ID);
-        	do{
-        		String title = cursor.getString(titleCol);
-        		Log.d("Title:", title);
-        		
-        		long id= cursor.getLong(idCol);
-        		Log.d("Id:", Long.toString(id));
-        		
-        		Uri uri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
-        		Log.d("URI", uri.toString());
-        		
-        	}while(cursor.moveToNext());
-        }
         	
+        	cursor.move(188);
+        	
+        	mCurrTrackTitle = cursor.getString(titleCol);
+    		
+    		long id= cursor.getLong(idCol);
+    		Uri uri = ContentUris.withAppendedId(android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI, id);
+    		Log.d("URI", uri.toString());
+    	    	
+    		try {
+    			mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+    			mPlayer.setDataSource(getApplicationContext(), uri);
+    			mPlayer.prepare();
+    			mPlayer.start();        		
+			} catch (IOException e) {
+				Log.e("minstrel.WelcomActivity", e.toString());
+			}
+    		        	
+        }
         
-        
-        
-        setContentView(R.layout.welcome);
+     
     }
 
     @Override
@@ -54,5 +73,11 @@ public class WelcomeActivity extends Activity {
         getMenuInflater().inflate(R.menu.welcome, menu);
         return true;
     }
+
+	@Override
+	public void onClick(View v) {
+		String entry = mCurrTrackTitle+": "+mPlayer.getCurrentPosition()+" / "+mPlayer.getDuration();
+		mBookMarkList.append(entry+"\n");		
+	}
     
 }
